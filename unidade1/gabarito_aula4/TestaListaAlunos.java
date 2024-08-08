8f6a2c4a980544d393dc026a257546a6dc2ce334
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-08 08:07:02 -0300

    Aguste

--- unidade1/gabarito_aula4/TestaListaAlunos.java
+++ unidade1/gabarito_aula4/TestaListaAlunos.java
@@ -5,13 +5,15 @@ import java.util.LinkedList;
 public class TestaListaAlunos {
     public static void main(String[] args) {
         LinkedList<Aluno> osAlunos = new LinkedList<Aluno>();
+        
         for (int i = 0; i < 89764; i++) {
             Aluno umAluno = new Aluno();
 
             umAluno.setNome("Aluno Nº" + i);
             osAlunos.add(umAluno);
             System.out.println(umAluno);
-
         }
+
+        System.out.println("Imprimindo a lista de Alunos; ");
     }
 }
