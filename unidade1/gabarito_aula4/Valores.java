9586e07a2b8b168a08c7677ba72d2609e027b78f
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-08 09:39:21 -0300

    Valores

---
+++ unidade1/gabarito_aula4/Valores.java
@@ -0,0 +1,12 @@
+package unidade1.gabarito_aula4;
+
+public class Valores {
+    static int max = Integer.MAX_VALUE;
+    static int min = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        System.out.println(max);
+        System.out.println(min);
+    }
+
+}
