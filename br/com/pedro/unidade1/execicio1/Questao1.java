a3354e39aa03d2c99810e4db8f8eb978c75d2742
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-16 00:19:56 -0300

    testes 1

---
+++ br/com/pedro/unidade1/execicio1/Questao1.java
@@ -0,0 +1,40 @@
+/**
+Exercicio 1/questao 1 = Corrigir Erros 
+@author pedroBezerra
+@version 1.0
+@since 2024-08-15
+@codigo-secreto 342492419
+*/
+package br.com.pedro.unidade1.execicio1;
+
+import java.util.Scanner;
+
+public class Questao1 {
+
+    public double getDistancia() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        double x1, y1, x2, y2, distancia = 1.0;
+
+        System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
+        x1 = teclado.nextDouble();
+        y1 = teclado.nextDouble();
+        x2 = teclado.nextDouble();
+        y2 = teclado.nextDouble();
+
+        distancia = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), distancia);
+
+        System.out.println("A distância é: " + distancia);
+        return distancia;
+      
+    }
+  
+
+    public static void main(String[] args) {
+      System.out.println(distancia);
+        sysout(getDistancia());
+        Scanner teclado;
+        teclado.close();
+    }
+}
