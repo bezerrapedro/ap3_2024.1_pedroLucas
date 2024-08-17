3cb1719eac66406383439369160838af5fd512c8
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-17 17:11:48 -0300

    Questão 1

--- br/com/pedro/unidade1/execicio1/Questao1.java
+++ br/com/pedro/unidade1/execicio1/Questao1.java
@@ -11,30 +11,45 @@ import java.util.Scanner;
 
 public class Questao1 {
 
+    double x1, y1, x2, y2, distancia = 1.0, amplitude = 1.0;
+
     public double getDistancia() {
 
         Scanner teclado = new Scanner(System.in);
-
-        double x1, y1, x2, y2, distancia = 1.0;
 
         System.out.println("Entre com as coordenadas x e y dos pontos nesta ordem:");
         x1 = teclado.nextDouble();
         y1 = teclado.nextDouble();
         x2 = teclado.nextDouble();
         y2 = teclado.nextDouble();
-
-        distancia = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), distancia);
+        teclado.close();
+        distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
         System.out.println("A distância é: " + distancia);
         return distancia;
-      
+
     }
-  
+
+    public double getAmpltude() {
+
+        double xA1 = this.x1;
+        double xA2 = this.x2;
+        double yA1 = this.y1;
+        double yA2 = this.y2;
+
+        double amplitudeX = Math.abs(xA2 - xA1);
+        double amplitudeY = Math.abs(yA2 - yA1);
+
+        System.out.println("A amplitude X é: " + amplitudeX);
+        System.out.println("A amplitude Y é: " + amplitudeY);
+
+        return amplitudeX + amplitudeY;
+
+    }
 
     public static void main(String[] args) {
-      System.out.println(distancia);
-        sysout(getDistancia());
-        Scanner teclado;
-        teclado.close();
+        Questao1 questao1 = new Questao1();
+        questao1.getDistancia();
+        questao1.getAmpltude();
     }
 }
