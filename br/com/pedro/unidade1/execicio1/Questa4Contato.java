9929d0a87cdc1ae4d71af04b69091bca801590e5
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-17 20:47:40 -0300

    1

---
+++ br/com/pedro/unidade1/execicio1/Questa4Contato.java
@@ -0,0 +1,12 @@
+/**
+Exercicio 1/questao 4 = Classe Contato  
+@author pedroBezerra
+@version 1.0
+@since 2024-08-17
+@codigo-secreto 342492419
+*/
+package br.com.pedro.unidade1.execicio1;
+
+public class Questa4Contato {
+    public Contato
+}
