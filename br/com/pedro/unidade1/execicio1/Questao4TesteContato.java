4f3098e544472ffa04e1b6fbf8acc03fd8be75fe
bezerrapedro <obezerra.pedro@gmail.com>
2024-08-18 15:19:17 -0300

    pintores renascentistas

--- br/com/pedro/unidade1/execicio1/Questao4TesteContato.java
+++ br/com/pedro/unidade1/execicio1/Questao4TesteContato.java
@@ -10,9 +10,9 @@ package br.com.pedro.unidade1.execicio1;
 public class Questao4TesteContato {
 
     public static void main(String[] args) {
-        Questao4Contato contato1 = new Questao4Contato("Pedro Lucas", "(84) 99999-8888");
-        Questao4Contato contato2 = new Questao4Contato("Cleiton Rasta", "(84) 77777-5555");
-        Questao4Contato contato3 = new Questao4Contato("Nego Drama", "(84) 44157-3157");
+        Questao4Contato contato1 = new Questao4Contato("Caravaggio", "(84) 99999-8888");
+        Questao4Contato contato2 = new Questao4Contato("Leonardo da Vinci", "(84) 77777-5555");
+        Questao4Contato contato3 = new Questao4Contato("Donatello", "(84) 44157-3157");
 
         System.out.println("Contato numero 1 = Nome: " + contato1.getNome() + ", Numero: " + contato1.getTelefone());
         System.out.println("Contato numero 1 = Nome: " + contato2.getNome() + ", Numero: " + contato2.getTelefone());
