bdc8f223ca264a5e02e23aa753f4cf567baf0512
bezerrapedro <obezerra.pedro@gmail.com>
2024-09-06 21:12:55 -0300

    Import

--- br/com/pedro/unidade2/exerciocio1/MainRestaurante.java
+++ br/com/pedro/unidade2/exerciocio1/MainRestaurante.java
@@ -1,4 +1,7 @@
-package br.com.pedro.unidade2.exerciocio1.restaurante_heranca;
+package br.com.pedro.unidade2.exerciocio1;
+
+import br.com.pedro.unidade2.exerciocio1.restaurante_heranca.Funcionario;
+import br.com.pedro.unidade2.exerciocio1.restaurante_heranca.Garcom;
 
 public class MainRestaurante {
     public static void main(String[] args) {
